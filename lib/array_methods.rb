f4424afbf0906e5212f05197e17e50a23e4a4dab
Sergio Robles <sroblesc@gmail.com>
2019-12-11 23:55:17 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,26 @@
+
+
+# takes in an array and a value and returns the index of that value:
+#return the index of the value
+#if the value is not found, return nil
+#loop over the array and compare each value in the array to the value provided(this is not the value to be provided)
+
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  if value_to_find
+  
+  return value_to_find.index
 end
+
+
+# takes in an array of integers and returns the highest value integer:
 
 def find_max_value(array)
-  # Add your solution here
+  
 end
 
+
+# takes in an array of integers and returns the lowest value integer:
+
 def find_min_value(array)
-  # Add your solution here
+  
 end
